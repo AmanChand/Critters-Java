ac2034ca12c4dcae386b89aea17bac2caddc0122
Aman Chand <amanchand45@gmail.com>
2016-06-21 11:21:34 -0700

    Update Critter.java

--- Critter.java
+++ Critter.java
@@ -1,13 +1,3 @@
-// Stuart Reges
-// 12/03/04
-//
-// The Critter interface defines the methods necessary for an animal
-// to participate in the critter simulation. It must return a
-// character when getChar is called that is used for displaying it on
-// the screen. The getMove method must return a legal move (one of
-// the constants NORTH, SOUTH, EAST, WEST, CENTER). See the
-// CritterInfo interface for a description of its methods.
-
 
 	public interface Critter {
  
